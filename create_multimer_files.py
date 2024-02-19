c3d07971472c2a20a2e13ad93bfc6c40572f6468
Quentin Rouger <144003341+Qrouger@users.noreply.github.com>
2024-02-19 09:55:46 +0100

    Script for create file .txt to be use with alphapulldown

---
+++ create_multimer_files.py
@@ -0,0 +1,16 @@
+import os
+print("Name of protein")
+name = input()
+print ("Numbers of homo-oligomers")
+n = input()
+def create_multimer_file(name,n) :
+	filename = "multimer_"+name+".txt"
+	with open(filename,"w") as fh :
+		for nbr_line in range(int(n)) :
+			sentence = name+","+str(nbr_line+1)+"\n"
+			fh.write(sentence)
+
+if __name__ == "__main__" :
+	create_multimer_file(name,n)
+
+
