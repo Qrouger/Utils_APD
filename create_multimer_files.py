a53ef3ebe239ab255ee20fcc68d88bbfedbc61d5
Quentin Rouger <144003341+Qrouger@users.noreply.github.com>
2024-03-28 15:15:56 +0100

    remove error if a line is empty

--- create_multimer_files.py
+++ create_multimer_files.py
@@ -9,8 +9,9 @@ def create_multimer_file(fasta_file) :
         with open(fasta_file, "r") as handle :
             for line in handle :
                line = line.strip().split()
-               if line[0][0] != ">" :
-                  lenght += len(line)
+	       if len(line) > 0 :
+                  if line[0][0] != ">" :
+                     lenght += len(line)
         num_multimer = 2000//lenght
         if num_multimer > 15 :
            num_multimer = 15
